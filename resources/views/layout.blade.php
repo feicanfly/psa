de2ee253c629870b9ba30275812e21f7d90c078a
wang pengfei <6225683@gmail.com>
2015-10-30 10:12:58 +0800

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>遇见你</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+
+<div class="container">
+	@yield('content')
+</div>
+
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+	<h1>遇见.</h1>
+@stop
