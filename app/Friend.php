8a3663157a69b0679e7ad4b9e2a57ae545844962
wang pengfei <6225683@gmail.com>
2015-10-31 16:17:04 +0800

    表名称小写

--- app/Friend.php
+++ app/Friend.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Friend extends Model
 {
-    protected $table = 'Friends';
+    protected $table = 'friends';
 
     /**
      * The attributes that are mass assignable.
--- app/Profile.php
+++ app/Profile.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    protected $table = 'Profile';
+    protected $table = 'profile';
 
     protected $fillable = ['name','user_id','avatar','gender','phone','age','address'];
 }
